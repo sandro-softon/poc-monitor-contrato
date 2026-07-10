74eb46ea9eb472238e21b8e1a6afa243eaf213f3
sandro-softon <sandro@softon.com.br>
2026-07-10 08:25:38 -0300

    chore: ajusta chunk size warning e separa vendor bundles

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1200,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          antd: ['antd', '@ant-design/icons'],
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
   server: {
     port: 5173,
     proxy: {
